a8c7bca999725c706db35ea621a094c1acef3375
j-belec <jeronimobelec@hotmail.com>
2023-01-05 21:35:26 -0300

    agrego actualizaciones para render

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
 const path = require("path");
+const port = process.env.port || 3001;
 
 app.use(express.static(path.join(__dirname, "./public")));
 
-app.listen(PORT, console.log("Servidor corriendo"));
+app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/index.html"));
